ecfcecd44d00bed64abd038903e4457927f38cc6
LucasM898 <lwmiller14@gmail.com>
2025-03-03 17:41:14 -0700

    js code for menu selectors

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
+document.addEventListener("scroll", function () {
+    let menu = document.getElementById("menu-selectors");
+    let scrollPosition = window.scrollY;
 
+    if (scrollPosition >= window.innerHeight * 0.25) { 
+        menu.style.position = "fixed";
+        menu.style.top = "0";
+    } else {
+        menu.style.position = "absolute";
+        menu.style.top = "25vh";
+    }
+});

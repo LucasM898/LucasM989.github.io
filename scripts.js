8ce856ea07ca081cbfe0cab6fe9f96dcf8cbda33
LucasM898 <lwmiller14@gmail.com>
2025-03-03 17:57:53 -0700

    Fixed the menu selectors on mobile, and added background

--- scripts.js
+++ scripts.js
@@ -1,12 +1,14 @@
 document.addEventListener("scroll", function () {
     let menu = document.getElementById("menu-selectors");
     let scrollPosition = window.scrollY;
+    let screenWidth = window.innerWidth; 
+    let topValue = screenWidth < 700 ? "10vh" : screenWidth < 1300 ? "17.5vh" : "25vh";
 
     if (scrollPosition >= window.innerHeight * 0.25) { 
         menu.style.position = "fixed";
         menu.style.top = "0";
     } else {
         menu.style.position = "absolute";
-        menu.style.top = "25vh";
+        menu.style.top = topValue;
     }
 });

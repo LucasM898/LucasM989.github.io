7bcc7fe5f24167a50322ca0091116210e7bb4340
LucasM898 <lwmiller14@gmail.com>
2025-03-27 08:21:11 -0700

    jkjfg

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ document.addEventListener("scroll", function () {
     let menu = document.getElementById("navbar");
     let scrollPosition = window.scrollY;
     let screenWidth = window.innerWidth;
-    let topValue = screenWidth < 700 ? "10vh" : screenWidth < 1300 ? "17.5vh" : "25vh";
+    let topValue = screenWidth < 700 ? "15vh" : screenWidth < 1300 ? "17.5vh" : "25vh";
 
     if (scrollPosition >= window.innerHeight * 0.25) {
         menu.style.position = "fixed";
